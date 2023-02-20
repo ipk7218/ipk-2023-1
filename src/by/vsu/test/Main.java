5de64bc4406f4d7c318b30f4555eb997f4ac6cc0
ipk7218 <ialexandr7218@gmail.com>
2023-02-20 20:25:16 +0300

    Change Type of Inputs

--- src/by/vsu/test/Main.java
+++ src/by/vsu/test/Main.java
@@ -7,14 +7,14 @@ import by.vsu.domain.Product;
 public class Main {
 	public static void main(String[] args) {
 		Category category1 = new Category();
-		category1.setName("Молочная продукция");
+		category1.setName("Кустарники");
 		Category category2 = new Category();
-		category2.setName("Мясная продукция");
+		category2.setName("Букеты");
 
-		Product product1 = new Product("Молоко", new Currency(2L, 87), category1);
-		Product product2 = new Product("Рулька", new Currency(16L, 76));
+		Product product1 = new Product("Барбарис", new Currency(2L, 87), category1);
+		Product product2 = new Product("Розы", new Currency(16L, 76));
 		product2.setCategory(category2);
-		Product product3 = new Product("Творог", new Currency(3L, 33), category1);
+		Product product3 = new Product("Шиповник", new Currency(3L, 33), category1);
 
 		System.out.println(product1);
 		System.out.println(product2);
